e5ede182556785b55843d73226cdcda6d7a52375
Wappler <git@wappler.io>
2024-12-29 20:09:00 -0300

    Domingo 29/12

--- public/dmxAppConnect/config.js
+++ public/dmxAppConnect/config.js
@@ -31,12 +31,87 @@ dmx.config({
       },
       {
         "type": "boolean",
-        "name": "desconto"
+        "name": "tem_cupom"
       },
       {
-        "type": "number",
-        "name": "valor_desconto"
+        "type": "text",
+        "name": "codigo_cupom"
       }
     ]
+  },
+  "index": {
+    "itens_venda": {
+      "meta": [
+        {
+          "type": "number",
+          "name": "id"
+        },
+        {
+          "type": "datetime",
+          "name": "data_criacao"
+        },
+        {
+          "type": "number",
+          "name": "usuario_id"
+        },
+        {
+          "type": "number",
+          "name": "evento_id"
+        },
+        {
+          "type": "text",
+          "name": "produto_nome"
+        },
+        {
+          "type": "number",
+          "name": "qtide_cadastrada"
+        },
+        {
+          "type": "datetime",
+          "name": "inicio_vendas"
+        },
+        {
+          "type": "datetime",
+          "name": "final_vendas"
+        },
+        {
+          "type": "number",
+          "name": "valor"
+        },
+        {
+          "type": "number",
+          "name": "ordem_lista"
+        },
+        {
+          "type": "boolean",
+          "name": "ativo"
+        },
+        {
+          "type": "boolean",
+          "name": "is_mesa"
+        },
+        {
+          "type": "number",
+          "name": "qtide_mesa"
+        },
+        {
+          "type": "boolean",
+          "name": "cortesia"
+        },
+        {
+          "type": "boolean",
+          "name": "cupom_ativo"
+        },
+        {
+          "type": "text",
+          "name": "codigo_cupom"
+        },
+        {
+          "type": "number",
+          "name": "valor_desconto"
+        }
+      ],
+      "outputType": "array"
+    }
   }
 });
